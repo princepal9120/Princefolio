fe30222aa10c140a36c9ca56c0ad586088fc37b3
Prince Pal <princepal9120@gmail.com>
2024-10-05 00:28:46 +0530

    resume and image upload fresh

--- src/Helpers/ProjectsData.js
+++ src/Helpers/ProjectsData.js
@@ -3,13 +3,13 @@ import Port2 from "../assets/images/Currency-converter.png";
 import Port3 from "../assets/images/LandingWebiste.png";
 import Port4 from "../assets/images/HireHub.png";
 import Port5 from "../assets/images/Interviewai.png";
-// import Port6 from "../assets/images/Admin Dashboard.png";
+import Port6 from "../assets/images/image.png";
 
 export const ProjectData = [
   {
     title:"Jop Portal",
     image: Port4,
-    url: "https://hire-hub-three.vercel.app/",
+    url: "https://talentbridge-1yxp.onrender.com/",
     animationDelay: "w-4",
     tools: "Reacjs,Nodejs,Express,MongoDB",
     id: 4,
@@ -24,21 +24,21 @@ export const ProjectData = [
     id: 1,
   },
  
-  // {
-  //   title: "Admin Dashboard",
-  //   image: Port6,
-  //   url: "https://paks-admin-dashboard.netlify.app/",
-  //   animationDelay: "w-6",
-  //   tools: "REACT, SASS, RECHARTS, MUI",
-  //   id: 6,
-  // },
+  {
+    title: "EcoQuest",
+    image: Port6,
+    url: "https://github.com/princepal9120/EcoQuest",
+    animationDelay: "w-6",
+    tools: "REACT, SASS, RECHARTS, MUI",
+    id: 6,
+  },
  
   {
     title: "InterviewAi",
     image: Port5,
     url: "https://interviewai-smoky.vercel.app/",
     animationDelay: "w-6",
-    tools: "REACT, SASS, RECHARTS, MUI",
+    tools: "NextJs, NodeJs ,Express,Drizzle ORM,NeonDb",
     id: 5,
   },
   {
@@ -46,7 +46,7 @@ export const ProjectData = [
     image: Port3,
     url: "https://course-landing-website--mu.vercel.app",
     animationDelay: "w-3",
-    tools: "REACT, Aceternity UI",
+    tools: "NextJs, Aceternity UI",
     id: 3,
   },
   {
