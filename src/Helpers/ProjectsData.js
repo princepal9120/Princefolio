c790134c48c3ba26dae96b2f46499400fa9db8f5
Prince Pal <princepal9120@gmail.com>
2024-10-05 00:33:14 +0530

    project place change

--- src/Helpers/ProjectsData.js
+++ src/Helpers/ProjectsData.js
@@ -7,7 +7,7 @@ import Port6 from "../assets/images/image.png";
 
 export const ProjectData = [
   {
-    title:"Jop Portal",
+    title: "Jop Portal",
     image: Port4,
     url: "https://talentbridge-1yxp.onrender.com/",
     animationDelay: "w-4",
@@ -19,26 +19,27 @@ export const ProjectData = [
     title: "BlogHub",
     image: Port1,
     url: "https://blog-hub-1.vercel.app/",
-    animationDelay: "w-5",
+    animationDelay: "w-6",
     tools: "REACT, Javascript, Appwrite, Tailwind CSS",
+    id: 6,
+  },
+
+  {
+    title: "InterViewAi",
+    image: Port5,
+    url: "https://interviewai-smoky.vercel.app/",
+
+    animationDelay: "w-5",
+    tools: "REACT, SASS, RECHARTS, MUI",
     id: 1,
   },
- 
+
   {
     title: "EcoQuest",
     image: Port6,
     url: "https://github.com/princepal9120/EcoQuest",
     animationDelay: "w-6",
-    tools: "REACT, SASS, RECHARTS, MUI",
-    id: 6,
-  },
- 
-  {
-    title: "InterviewAi",
-    image: Port5,
-    url: "https://interviewai-smoky.vercel.app/",
-    animationDelay: "w-6",
-    tools: "NextJs, NodeJs ,Express,Drizzle ORM,NeonDb",
+    tools: "NextJs, Drizzle ORM,NeonDb,Gemini Api",
     id: 5,
   },
   {
@@ -57,8 +58,4 @@ export const ProjectData = [
     tools: "React, CSS, JavaScript",
     id: 2,
   },
-
-
-
- 
 ];
